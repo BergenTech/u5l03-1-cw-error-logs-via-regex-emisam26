564e120f94b03fbd2061feaf4e866e46c542061c
emisam26 <116697508+emisam26@users.noreply.github.com>
2024-03-12 12:58:22 +0000

    assignment completed

--- error.py
+++ error.py
@@ -1,2 +1,28 @@
+import json, re, csv
 
-# d
+try:
+    with open("logs.json") as jsonFile:
+        logs_py = json.load(jsonFile)
+        # print(logs_py[0]['level'])
+        error_logs = []
+        # error_logs_num = 0
+        for log in logs_py:
+            pattern = r"ERROR"
+            match = re.search(pattern,log['level'],re.IGNORECASE)
+            if match:
+                error_logs.append(log)
+        #         error_logs_num += 1
+        # print(error_logs_num)
+        try:
+            with open("error_logs.csv",'w',newline='') as csvFile:
+                writer = csv.writer(csvFile)
+                headers = ['timestamp','level','message']
+                writer.writerow(headers)
+                for log in error_logs:
+                    templist = [log[header] for header in headers]
+                    writer.writerow(templist)
+                    templist.clear()
+        except:
+            print("Error occured")
+except FileNotFoundError:
+    print("Error occured: File not found")
